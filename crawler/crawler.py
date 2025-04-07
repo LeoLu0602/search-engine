fb4e431427fb8512f41df2f323f39422bd99d507
Leo <96434243+LeoLu0602@users.noreply.github.com>
2025-04-06 22:14:09 -0400

    feat: finish url discovery

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,4 +1,12 @@
+import requests
 from bs4 import BeautifulSoup
+import json
+import re
+import time
+import random
+
+MAX_DEPTH = 2
+TEXT_LEN = 200
 
 
 def read_seed_urls():
@@ -14,7 +22,48 @@ def read_seed_urls():
 
 
 def bfs(seed_urls):
-    urls = []
+    urls = {}
+    depth = 0
+    queue = seed_urls
+
+    while len(queue) > 0 and depth < MAX_DEPTH:
+        new_queue = []
+
+        for url in queue:
+            url = url.rstrip("/")
+
+            # filter out: url that is not started with https:// or http://
+            # url is guaranteed to be new
+            if not (url.startswith("https://") or url.startswith("http://")):
+                continue
+
+            time.sleep(random.uniform(0, 1))
+            response = requests.get(url)
+            soup = BeautifulSoup(response.content, "html.parser")
+            lang = soup.html.get('lang')
+
+            # only crawl english sites
+            if lang != "en":
+                continue
+
+            title = soup.title.string if soup.title else ""
+            text = re.sub(r"\s+", " ", soup.get_text()).strip()[:TEXT_LEN]
+            urls[url] = {"url": url, "title": title, "text": text, "in_degree": 0}
+            print(f"{url}")
+
+            for a_tag in soup.find_all("a", href=True):
+                link = a_tag["href"].rstrip("/")
+
+                if link in urls:
+                    urls[link]["in_degree"] += 1
+
+                    # link was visited before -> don't put it into queue again
+                    continue 
+
+                new_queue.append(link)
+
+        depth += 1
+        queue = new_queue
 
     return urls
 
@@ -22,6 +71,11 @@ def bfs(seed_urls):
 def main():
     print("reading from seed_urls.txt")
     seed_urls = read_seed_urls()
+    print("start crawling")
+    urls = bfs(seed_urls[:])
+
+    with open("urls.json", "w") as f:
+        json.dump(urls, f, indent=4)
 
 
 if __name__ == "__main__":
