438264b3f970f97ae4895b1b5e6d5100c52487ff
Leo <96434243+LeoLu0602@users.noreply.github.com>
2025-04-08 22:06:25 -0400

    feat: build index

--- data-collection/indexing.py
+++ data-collection/indexing.py
@@ -1,23 +1,44 @@
 import json
+import nltk  # type: ignore
+from nltk.corpus import stopwords  # type: ignore
+
+nltk.download("stopwords")
+STOP_WORDS = set(stopwords.words("english"))
 
 
 def read_urls():
-    with open("./crawler/urls.json", "r") as f:
+    with open("./urls.json", "r") as f:
         return json.load(f)
 
 
 def process_urls(urls):
     index = {}
-    
+
     for url in urls:
-        pass
+        for token in url["content"]:
+            token = token.lower()
+
+            # we don't need stop words
+            if token in STOP_WORDS:
+                continue
+
+            if token not in index:
+                index[token] = []
+
+            index[token].append(url["url"])
+
+    print(f"{len(index.keys())} tokens")
 
     return index
 
+
 def main():
-    print("read from ./crawler/urls.json")
+    print("read from urls.json")
     urls = read_urls()
     index = process_urls(urls)
+
+    with open("./index.json", "w") as f:
+        json.dump(index, f, indent=4)
 
 
 if __name__ == "__main__":
