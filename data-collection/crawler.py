323fc5ca8ff5acf3646924fe5326a2c9c2b2a476
leolu0602 <lyuholu@gmail.com>
2025-04-08 20:38:09 -0400

    refactor: move logic inside bfs to main and crawl

--- data-collection/crawler.py
+++ data-collection/crawler.py
@@ -21,58 +21,56 @@ def extract_tokens(text, count):
     return tokens[:count]
 
 
-def bfs(seed_urls):
+def crawl(visited, to_be_visited, depth):
     urls = []
-    url_visited = set()
-    depth = 0
-    to_be_visited = seed_urls
+    new_to_be_visited = []
 
-    while len(to_be_visited) > 0:
-        tmp = []  # to_be_visited for the next iteration
+    for url in to_be_visited:
+        url = url.rstrip("/")
 
-        for url in to_be_visited:
-            url = url.rstrip("/")
+        # filter out:
+        # 1. duplicate
+        # 2. url that is not started with https:// or http://
+        if url in visited or not (
+            url.startswith("https://") or url.startswith("http://")
+        ):
+            continue
 
-            # filter out:
-            # 1. duplicate
-            # 2. url that is not started with https:// or http://
-            if url in url_visited or not (
-                url.startswith("https://") or url.startswith("http://")
-            ):
-                continue
+        time.sleep(random.uniform(0, 0.5))
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, "html.parser")
 
-            time.sleep(random.uniform(0, 0.5))
-            response = requests.get(url)
-            soup = BeautifulSoup(response.content, "html.parser")
+        if not soup.html or soup.html.get("lang") != "en":
+            continue
 
-            if not soup.html or soup.html.get("lang") != "en":
-                continue
+        title = soup.title.get_text() if soup.title else ""
+        body_text = soup.body.get_text() if soup.body else ""
+        content = extract_tokens(body_text, 200)
+        urls.append({"url": url, "title": title, "content": content})
+        visited.add(url)
+        print(f"#{len(urls)} (depth: {depth}) {url}")
 
-            title = soup.title.get_text() if soup.title else ""
-            body_text = soup.body.get_text() if soup.body else ""
-            content = extract_tokens(body_text, 200)
-            urls.append({"url": url, "title": title, "content": content})
-            url_visited.add(url)
-            print(f"#{len(urls)} {url} (depth: {depth})")
+        for a_tag in soup.find_all("a", href=True):
+            link = a_tag["href"].rstrip("/")
+            new_to_be_visited.append(link)
 
-            for a_tag in soup.find_all("a", href=True):
-                link = a_tag["href"].rstrip("/")
-                tmp.append(link)
-
-        depth += 1
-        to_be_visited = tmp
-
-        if depth > MAX_DEPTH:
-            break
-
-    return urls
+    return urls, new_to_be_visited
 
 
 def main():
     print("read from seed_urls.txt")
     seed_urls = read_seed_urls()
     print("start crawling")
-    urls = bfs(seed_urls[:])
+
+    urls = []  # a list of {url, title, content}
+    visited = set()  # a set of url
+    to_be_visited = seed_urls[:]
+    depth = 0
+
+    while depth <= MAX_DEPTH and len(to_be_visited) > 0:
+        urls_partial, to_be_visited = crawl(visited, to_be_visited, depth)
+        urls += urls_partial
+        depth += 1
 
     with open("urls.json", "w") as f:
         json.dump(urls, f, indent=4)
