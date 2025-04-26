fac959d5a28e8ffcbdb20369459e59b3eaf52c43
Leo Lu <lyuholu@gmail.com>
2025-04-25 23:31:25 -0400

    feat: set up client

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='w-full h-screen flex flex-col gap-8 justify-center items-center'>
+      <h1 className='font-bold text-5xl block'>Search Engine</h1>
+      <input className='bg-[#ecedef] w-96 p-4 rounded-full focus:outline-none' />
+    </div>
+  )
+}
+
+export default App
